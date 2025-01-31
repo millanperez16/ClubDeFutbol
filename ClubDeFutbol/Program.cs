4ce6d2f48728b2f117a3eb3c06f3ea346641f47d
millanperez16 <ismaelmillanperez@gmail.com>
2025-01-31 13:10:23 +0100

    Prueba 1 Hola mundo.Agregar archivos de proyecto.

---
+++ ClubDeFutbol/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// cara culo
+Console.WriteLine("Hola mundo! :)");
+Console.WriteLine("Github cabron");
