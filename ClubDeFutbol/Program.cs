e6b8cc57120ffa30367a19934a325a5beebd4304
ASD-360 <android64sade@gmail.com>
2025-01-31 13:35:04 +0100

    Prueba 1

--- ClubDeFutbol/Program.cs
+++ ClubDeFutbol/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// cara culo
+
 Console.WriteLine("Hola mundo! :)");
 Console.WriteLine("Github cabron");
+Console.WriteLine("Github cabron");
+
