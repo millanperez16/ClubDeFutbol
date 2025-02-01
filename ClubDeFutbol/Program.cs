56e50fc84f410f169d2a0729db2e4d974cebcbb1
millanperez16 <ismaelmillanperez@gmail.com>
2025-02-01 21:08:42 +0100

    Switch añadido

--- ClubDeFutbol/Program.cs
+++ ClubDeFutbol/Program.cs
@@ -1,5 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Patata");
-Console.WriteLine("ayuda");
-Console.WriteLine("Campo del fulvo");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int op = -1;
+
+        while (op != 0) 
+        { 
+            Console.WriteLine("Gestión del club");
+            Console.WriteLine("¿Qué quieres hacer?");
+            Console.WriteLine("1. Añadir equipos");
+            Console.WriteLine("2. Añadir equipos");
+            Console.WriteLine("3. Añadir equipos");
+            Console.WriteLine("4. Añadir equipos");
+            op = Convert.ToInt32(Console.ReadLine());
+            switch (op) 
+            {
+                case 0:
+                    Console.WriteLine("Adiós");
+                    break;
+                default:
+                    Console.WriteLine("Prueba otra opción");
+                    break;
+            }
+        }
+    }
+}
